b995230d1858b805493feda3514c731c7dbde808
Christian E. Fonkeng <46832200+fonkengChris@users.noreply.github.com>
2023-04-25 22:58:03 +0100

    Built game trailer

--- src/pages/GameDetialPage.tsx
+++ src/pages/GameDetialPage.tsx
@@ -6,6 +6,7 @@ import CriticScore from "../components/CriticScore";
 import DefinitionItem from "../components/DefinitionItem";
 import ExpandableText from "../components/ExpandableText";
 import GameAttributes from "../components/GameAttributes";
+import GameTrailer from "../components/GameTrailer";
 import useGame from "../hooks/useGame";
 
 const GameDetialPage = () => {
@@ -20,6 +21,7 @@ const GameDetialPage = () => {
       <Heading>{game.name}</Heading>
       <ExpandableText>{game.description_raw}</ExpandableText>
       <GameAttributes game={game} />
+      <GameTrailer gameId={game.id}/>
     </>
   );
 };
